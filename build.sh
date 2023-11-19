5e8cb29c01ea53e570b5275c181e7c753c5a5c43
Pawel Bogut <pbogut@pbogut.me>
2023-11-19 15:55:56 +0100

    feat: new homepage in zola

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#=================================================
-# name:   build.sh
-# author: Pawel Bogut <http://pbogut.me>
-# date:   31/01/2018
-#=================================================
-docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll jekyll build $@
