31b03e1540ed172971eee79f3e31a8710876d90f
Pawel Bogut <pbogut@pbogut.me>
2023-12-19 11:37:35 +0100

    feat: add animation to skill progress bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./templates/**/*.html", "./theme/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        skill: 'skill 0.5s ease-in-out',
+      },
+      keyframes: {
+        skill: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
